c5428bb0330a75da0ebdcc57f48a8bd3d2643bad
NoxZet <noxiel.zet@gmail.com>
2021-08-08 17:43:57 +0200

    GameObject and RenderObject changes
    GO: improve properties for passing to model
    RO: remove setters for properties taken from GO, handle color from GO

--- source/Graphics/RenderObject.hpp
+++ source/Graphics/RenderObject.hpp
@@ -12,32 +12,15 @@ namespace Graphics {
 		public:
 			RenderObject(std::shared_ptr<Common::GameObject> commonPtr, IModel *model): commonObject(commonPtr), model(model) {};
 			void draw(Mtx view) override;
-			void setPosition(f32 x, f32 y, f32 z) override {
-				this->x = x;
-				this->y = y;
-				this->z = z;
-			}
-			void setColor(u8 cr, u8 cg, u8 cb) override {
-				color[0] = cr;
-				color[1] = cg;
-				color[2] = cb;
-			}
-			void setColor(u32 color) override {
-				// Assign u32 value to an array of u8 by casting it to array of u32
-				color = *((u32*)color);
-			}
-			void setAlpha(u8 ca) override {
-				color[3] = ca;
-			}
-			void setScale(f32 sx, f32 sy, f32 sz) override {
-				this->sx = sx;
-				this->sy = sy;
-				this->sz = sz;
+			void setColor(u8 cr, u8 cg, u8 cb, u8 ca) {
+				u8 *colorArr = (u8*)(&color);
+				colorArr[0] = cr;
+				colorArr[1] = cg;
+				colorArr[2] = cb;
+				colorArr[3] = ca;
 			}
 		private:
-			f32 x, y, z;
-			u8 color[4] = {0, 0, 0, 0xff};
-			f32 sx, sy, sz;
+			u32 color;
 			std::shared_ptr<Common::GameObject> commonObject;
 			IModel *model;
 	};
