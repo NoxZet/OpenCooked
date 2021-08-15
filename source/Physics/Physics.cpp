aaec02182ea944add8bad28159d785b67c25b066
NoxZet <noxiel.zet@gmail.com>
2021-08-15 11:32:14 +0200

    Remove deleted objects from vectors fix pointer errors
    - without triggering undefined behavior
    - because of the interleaved object deallocation and vector removal,
      I wrote my own loop to do this

--- source/Physics/Physics.cpp
+++ source/Physics/Physics.cpp
@@ -21,18 +21,17 @@ void Physics::tick() {
 }
 
 void Physics::clearObjects() {
-	// - See https://en.wikipedia.org/wiki/Erase–remove_idiom
-	// - In a nutshell, remove_if moves all elements that match the predicate to the end of the
-	//   vector and returns a pointer to the first of these elements, it keeps the vector's length
-	auto deleteIt = std::remove_if(objects.begin(), objects.end(), [](IPhysicsObject *object) {
-		return object->isDeleted();
-	});
-	// - Then we deallocate the RenderObjects
-	for (auto deallocIt = deleteIt; deallocIt != objects.end(); deallocIt++) {
-		delete *deallocIt;
+	// We deallocate the IPhysicsObjects while moving all following objects back to fill in the gaps created
+	u32 deleted = 0;
+	for (u32 i = 0; i < objects.size(); i++) {
+		objects[i - deleted] = objects[i];
+		if (objects[i]->isDeleted()) {
+			delete objects[i];
+			deleted++;
+		}
 	}
-	// - Erase removes all elements past the input pointer
-	objects.erase(deleteIt, objects.end());
+	// Remove the elements at the end (if there's valid objects in them, they were moved back in the previous step)
+	objects.erase(objects.end() - deleted, objects.end());
 }
 
 void Physics::newObject(std::shared_ptr<Common::GameObject> commonPtr) {
